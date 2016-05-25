70f0172d2aa9f4d9f1381f9cbe9791eca77d4cf2
Marco Milani <marco.milani@giuneco.it>
2016-05-25 14:37:42 +0200

    [START]

---
+++ Mvb/Mvb.Cross/IUiRunner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mvb.Cross
+{
+    public interface IUiRunner
+    {
+        /// <summary>
+        /// Execute action on UiThread
+        /// </summary>
+        /// <param name="action"></param>
+        void Run(Action action);
+    }
+}
