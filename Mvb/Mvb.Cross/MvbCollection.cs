a35e11334b94c47c34c5dd5b40f83e0c94e1b8f2
Marco Milani <marco.milani@giuneco.it>
2016-06-08 17:50:53 +0200

    [CROSS] Refactor on MvbCollection

--- Mvb/Mvb.Cross/MvbCollection.cs
+++ Mvb/Mvb.Cross/MvbCollection.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Mvb.Cross
 {
@@ -13,19 +10,21 @@ namespace Mvb.Cross
     {
         public MvbCollection()
             : base()
-        {
-        }
+        {}
 
         public MvbCollection(IEnumerable<T> collection)
             : base(collection)
-        {
-        }
+        {}
 
         public MvbCollection(List<T> list)
             : base(list)
-        {
-        }
+        {}
 
+		/// <summary>
+		/// Adds the range.
+		/// </summary>
+		/// <returns>The range.</returns>
+		/// <param name="range">Range.</param>
         public void AddRange(IEnumerable<T> range)
         {
             var startIndexd = this.Items.Count-1;
@@ -36,14 +35,19 @@ namespace Mvb.Cross
 
             this.OnPropertyChanged(new PropertyChangedEventArgs("Count"));
             this.OnPropertyChanged(new PropertyChangedEventArgs("Item[]"));
-            //this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
             this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, range.ToList(), startIndexd));
         }
 
-        public void Reset(IEnumerable<T> range)
+		/// <summary>
+		/// Reset the collection and addrange the specified range.
+		/// This will fire a NotifyCollectionChanged for reset followed by a NotifyCollectionChanged for add
+		/// </summary>
+		/// <param name="range">Range.</param>
+		public void Reset(IEnumerable<T> range)
         {
             this.Items.Clear();
-            this.AddRange(range);
+			this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+			this.AddRange(range);
         }
     }
 }
