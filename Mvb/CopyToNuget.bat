3f6adc92695bc680dd89eed9629f674a3bc152ca
Marco Milani <marco.milani@giuneco.it>
2016-08-16 22:12:07 +0200

    Update copy nuget

--- Mvb/CopyToNuget.bat
+++ Mvb/CopyToNuget.bat
@@ -1,13 +1,13 @@
-xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Cross.dll" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Pcl\"
-xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Cross.pdb" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Pcl\"
+xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Core.dll" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Pcl\"
+xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Core.pdb" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Pcl\"
 
-xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Cross.dll" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Droid\"
-xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Cross.pdb" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Droid\"
+xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Core.dll" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Droid\"
+xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Core.pdb" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Droid\"
 xcopy /s "C:\Projects\MVB\Mvb\Mvb.Droid\bin\Debug\Mvb.Droid.dll" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Droid\"
 xcopy /s "C:\Projects\MVB\Mvb\Mvb.Droid\bin\Debug\Mvb.Droid.pdb" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Droid\"
 
 
-xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Cross.dll" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Ios\"
-xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Cross.pdb" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Ios\"
+xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Core.dll" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Ios\"
+xcopy /s "C:\Projects\MVB\Mvb\Mvb.Cross\bin\Debug\Mvb.Core.pdb" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Ios\"
 xcopy /s "C:\Projects\MVB\Mvb\Mvb.Ios\bin\Debug\Mvb.Ios.dll" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Ios\"
 xcopy /s "C:\Projects\MVB\Mvb\Mvb.Ios\bin\Debug\Mvb.Ios.pdb" "C:\Users\marcogm.INT\Documents\Nuget DLL Sources\Mvb\Ios\"
