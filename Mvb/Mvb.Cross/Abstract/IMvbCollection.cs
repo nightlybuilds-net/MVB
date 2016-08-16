9997aa34fdd1994f3e73ce2158b373ac0f70193c
Marco Milani <marco.milani@giuneco.it>
2016-08-16 14:21:40 +0200

    Move every class on file

---
+++ Mvb/Mvb.Cross/Abstract/IMvbCollection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Specialized;
+using Mvb.Core.Args;
+
+namespace Mvb.Core.Components
+{
+    public interface IMvbCollection : INotifyCollectionChanged
+    {
+        event EventHandler<MvbCollectionItemChanged> MvbItemCollectionChanged;
+    }
+}
