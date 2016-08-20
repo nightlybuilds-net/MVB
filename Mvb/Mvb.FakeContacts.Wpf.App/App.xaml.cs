5ae5eac4226ce077326c7e79e915d36037ccd8f2
Marco Milani <marco.milani@giuneco.it>
2016-08-20 16:15:54 +0200

    Add WPF project

---
+++ Mvb/Mvb.FakeContacts.Wpf.App/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DryIoc;
+using Mvb.Core;
+using Mvb.FakeContacts.Abstract;
+using Mvb.FakeContacts.Concrete;
+using Mvb.FakeContacts.ModelBinders;
+using Mvb.FakeContacts.Win.Services;
+using Mvb = Mvb.Core.Mvb;
+
+namespace Mvb.FakeContacts.Wpf.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static IContainer Ioc;
+     
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            //Core.Mvb.NullInit();
+
+            UiRunnerDispenser.RegisterRunner(() => new WpfUiRunner());
+
+            //Init IOC
+            Ioc = new Container();
+
+            //Services
+            Ioc.Register<IContactServices, WinContactsServices>();
+            Ioc.Register<IAvatarServices, CommonAvatarServices>();
+
+            //ModelBinders
+            Ioc.Register<ContactsSummaryModelBinders>(Reuse.Singleton);
+            Ioc.Register<ContactsModelBinders>(Reuse.Singleton);
+        }
+    }
+}
---
+++ Mvb/Mvb.FakeContacts.Wpf.App/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DryIoc;
+using Mvb.Core.Args;
+using Mvb.FakeContacts.Domain;
+using Mvb.FakeContacts.ModelBinders;
+
+namespace Mvb.FakeContacts.Wpf.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ContactsModelBinders _contactsMb;
+        private readonly ContactsSummaryModelBinders _contactSummaryMb;
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this._contactsMb = App.Ioc.Resolve<ContactsModelBinders>();
+            this._contactSummaryMb = App.Ioc.Resolve<ContactsSummaryModelBinders>();
+
+            this.InitModelBinders();
+        }
+
+        private void LoadBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            this._contactsMb.LoadContacts();
+        }
+
+        private void ShakeBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            this._contactsMb.ShakeNames();
+        }
+
+
+        /// <summary>
+		/// Inits the model binders.
+		/// </summary>
+		private void InitModelBinders()
+        {
+            //Actions for 'IsBusy'
+            this._contactsMb.Binder.AddAction<ContactsModelBinders>(b => b.IsBusy, () =>
+            {
+                this.SummaryLbl.Background = this._contactsMb.IsBusy ? Brushes.Red : Brushes.Transparent;
+                this.LoadBtn.IsEnabled = !this._contactsMb.IsBusy;
+
+                //if (this._contactsMb.IsBusy)
+                //{
+                //    var snack = Snackbar.Make(this.FindViewById<TextView>(Resource.Id.SignatureTw), "Loading contacts..", Snackbar.LengthShort);
+                //    snack.View.SetBackgroundColor(Color.Red);
+                //    snack.Show();
+                //}
+            });
+
+            //Actions for 'Summary'
+            this._contactSummaryMb.Binder.AddAction<ContactsSummaryModelBinders>(b => b.Summary, () =>
+            {
+                this.SummaryLbl.Content = this._contactSummaryMb.Summary;
+            });
+            //MANUAL RUN FOR FIRST ASSIGNMENT
+            this._contactSummaryMb.Binder.Run<ContactsSummaryModelBinders>(b => b.Summary);
+
+            //Actions for 'Contacts' collections
+            this._contactsMb.Binder.AddActionForCollection<ContactsModelBinders>(b => b.Contacts, args =>
+            {
+                if (args.MvbUpdateAction == MvbUpdateAction.CollectionChanged)
+                {
+                    switch (args.NotifyCollectionChangedEventArgs.Action)
+                    {
+                        case NotifyCollectionChangedAction.Add:
+                            foreach (var newItem in args.NotifyCollectionChangedEventArgs.NewItems)
+                                this.ContactsListView.Items.Add(new ContactRow(((Contact)newItem).Name));
+                            this.LoadBtn.Visibility = Visibility.Collapsed;
+                            this.ShakeBtn.Visibility = Visibility.Visible;
+                            break;
+                        case NotifyCollectionChangedAction.Remove:
+                        case NotifyCollectionChangedAction.Replace:
+                        case NotifyCollectionChangedAction.Move:
+                            break;
+                        case NotifyCollectionChangedAction.Reset:
+                            this.ContactsListView.Items.Clear();
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+                else if (args.MvbUpdateAction == MvbUpdateAction.ItemChanged)
+                {
+                    var contact = (ContactRow)this.ContactsListView.Items[args.MvbCollectionItemChanged.Index];
+
+                    if (args.MvbCollectionItemChanged.PropertyName == "Name")
+                        contact.ContactName.Content = args.MvbCollectionItemChanged.NewValue;
+                    else
+                    {
+                        var bytes = new WebClient().DownloadData(args.MvbCollectionItemChanged.NewValue.ToString());
+
+                        var image = new BitmapImage();
+                        image.BeginInit();
+                        image.CacheOption = BitmapCacheOption.OnLoad;
+                        image.StreamSource = new MemoryStream(bytes);
+                        image.EndInit();
+                        contact.ContactImage.Source = image;
+                    }
+                    
+                }
+            });
+
+        }
+    }
+}
