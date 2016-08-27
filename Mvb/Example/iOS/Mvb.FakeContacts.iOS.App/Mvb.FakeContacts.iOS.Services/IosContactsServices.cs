9b348ced20abff2332ee589693ec23e9d7115e94
Marco Milani <emme.milani@hotmail.it>
2016-08-27 23:53:37 +0200

    Removed old projects. Clear folders

---
+++ Mvb/Example/iOS/Mvb.FakeContacts.iOS.App/Mvb.FakeContacts.iOS.Services/IosContactsServices.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AddressBook;
+using Mvb.FakeContacts.Abstract;
+using Mvb.FakeContacts.Domain;
+
+namespace Mvb.FakeContacts.iOS.Services
+{
+	public class IosContactsServices : IContactServices
+	{
+		public async Task<IEnumerable<Contact>> GetContacts()
+		{
+			return await Task.Run(() =>
+			{
+				var res = new List<Contact>();
+
+
+				var status = ABAddressBook.GetAuthorizationStatus();
+				if (status == ABAuthorizationStatus.Denied || status == ABAuthorizationStatus.Restricted)
+					return res;
+				else
+				{
+
+					var addressBook = new ABAddressBook();
+
+
+					var valorizeRes = new Action(() => 
+					{
+						var peoples = addressBook.GetPeople();
+
+						foreach (var item in peoples)
+						{
+							res.Add(new Contact
+							{
+								Name = $"{item.FirstName} {item.LastName}"
+							});
+						}
+					});
+
+
+					if (status == ABAuthorizationStatus.NotDetermined)
+					{
+						addressBook.RequestAccess((granted, e) =>
+					   {
+						   if (granted)
+								valorizeRes.Invoke();
+						   
+					   });
+					}
+					else
+					{
+						valorizeRes.Invoke();
+					}
+
+					return res;
+				}
+			});
+
+		}
+	}
+}
+
