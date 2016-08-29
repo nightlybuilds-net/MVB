587be69f4e986f7baa2f8ff148dc061317a3f4c3
Marco Milani <marco.milani@giuneco.it>
2016-08-29 12:40:58 +0200

    [FIX] Add MacOs proj to final

---
+++ Mvb/Example/MacOs/Mvb.FakeContacts.App.Cocoa/Mvb.FakeContacts.Cocoa.Services/CocoaContactServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Mvb.FakeContacts.Abstract;
+using Mvb.FakeContacts.Domain;
+
+namespace Mvb.FakeContacts.Cocoa.Services
+{
+	public class CocoaContactServices : IContactServices
+	{
+
+		public async Task<IEnumerable<Contact>> GetContacts()
+		{
+			return await Task.Run(() =>
+			{
+				return new List<Contact>
+				{
+					new Contact
+					{
+						Name = "Pippo"	
+					}
+				};
+
+			});
+		}
+	}
+}
+
