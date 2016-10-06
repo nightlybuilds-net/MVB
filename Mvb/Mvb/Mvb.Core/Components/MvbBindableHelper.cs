6fc08ffa9b9ce5226df0cd7d02d098a86e68bf2c
Marco Milani <marco.milani@giuneco.it>
2016-10-06 22:12:05 +0200

    [Add] MvbBindableHelper

---
+++ Mvb/Mvb/Mvb.Core/Components/MvbBindableHelper.cs
@@ -0,0 +1,21 @@
+﻿using Mvb.Core.Abstract;
+
+namespace Mvb.Core.Components
+{
+    public class MvbBindableHelper<T> where T : IMvbNotifyPropertyChanged
+    {
+        public T Item { get; private set; }
+
+        public void SetItem(T item)
+        {
+            this.Clear();
+            this.Item = item;
+        }
+
+        /// <summary>
+        /// Reset the state on SetItem
+        /// </summary>
+        public virtual void Clear()
+        {}
+    }
+}
