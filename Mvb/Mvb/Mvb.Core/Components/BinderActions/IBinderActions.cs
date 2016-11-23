413b3517a3151e3fa4f4381a2df1e0a96f3c81d1
Marco Milani <marco.milani@giuneco.it>
2016-11-23 11:36:09 +0100

    [CORE] Extract interface from MvbAction on generic

--- Mvb/Mvb/Mvb.Core/Components/BinderActions/IBinderActions.cs
+++ Mvb/Mvb/Mvb.Core/Components/BinderActions/IBinderActions.cs
@@ -7,12 +7,30 @@ namespace Mvb.Core.Components.BinderActions
         /// <summary>
         /// Add Action 
         /// </summary>
+        /// <param name="subscriber"></param>
         /// <param name="action"></param>
-        void AddAction(Action action);
+        void AddAction(object subscriber,Action action);
 
         /// <summary>
         /// Invoke all actions
         /// </summary>
         void Invoke();
     }
+
+    public interface IBinderActions<T>
+    {
+        /// <summary>
+        /// Add Action 
+        /// </summary>
+        /// <param name="subscriber"></param>
+        /// <param name="action"></param>
+        void AddAction(object subscriber,Action<T> action);
+
+        /// <summary>
+        /// Invoke all actions
+        /// </summary>
+        void Invoke(T arg);
+    }
+
+   
 }
